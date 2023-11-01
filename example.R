ccac2ce026f33115f0ba644f4946b0d8aca9be32
Zhuoran Li <zhl4010@med.cornell.edu>
2023-11-01 13:49:15 -0400

    change example

--- example.R
+++ example.R
@@ -1,6 +1,6 @@
 # add an example
 #install package from the repository "BoxplotPackage"
-devtools::install_github("Zhuoran-Li123/BoxplotPackage", force = TRUE)
+devtools::install_github("Zhuoran-Li123/Lab2", force = TRUE)
 # load the 'BoxplotPackage' package
 library(BoxplotPackage)
 # load the dataset "DRG_data" from the 'BoxplotPackage' package
@@ -8,3 +8,5 @@ library(readr)
 DRG_data <- read_csv("data/DRG_data.csv")
 # create a boxplot by boxplot_payment function
 boxplot_payment(DRG_data, 'Average Covered Charges')
+# create a calculates statistics over all of the DRG codes for average Medicare payments
+statistics_payment("Average Medicare Payments", DRG_data, "mean")
